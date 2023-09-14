36c0fe4ce37891a2f3f4cb280771d933f4b878c4
pemapol <t.pemapol@gmail.com>
2023-09-14 11:35:47 +0700

    add seeker config file and create dockerfile for seeker

---
+++ docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# docker-entrypoint.sh 
+### Original Script Configuration – or empty if simple command.
+…
+### Seeker Configuration
+if [[ ! -z ${SEEKER_ENABLED} ]]; then
+  rm -rf /var/lib/apt/lists/* && apt-get update && apt-get install curl -y 
+  curl -v -k -o seeker-agent.zip "${SEEKER_SERVER_URL}/rest/api/latest/installers/agents/binaries/PYTHON" 
+  mkdir -p /tmp/seeker && unzip seeker-agent.zip -d /tmp/seeker
+fi
+### Original Run Command or equivalent (potentially with new Seeker Arguments as required by the application configuration)
+### Should include potential CMD arguments provided to Docker if originally a simple command (accomplished here by $*) 
+
+#su webgoat -c "java $SEEKER_COMMAND -Djava.security.egd=file:/dev/./urandom -jar /home/webgoat/webgoat.jar $*"
